dd69bb22f0f017cd1da031bb3c154cb7651d0f2f
devyosse <narutoshippuden884@gmail.com>
2021-12-13 13:24:05 +0100

    projet carousel

---
+++ script.js
@@ -0,0 +1,60 @@
+let image = [
+    "url('img/img1.png')",
+    "url('img/img2.png')",
+    "url('img/img3.png')",
+    "url('img/img4.png')",
+    "url('img/img5.png')",
+]
+
+let Img = document.getElementsByTagName("img");
+let Btnleft = document.getElementById('beforebutton');
+let BtnRight = document.getElementById('afterbutton');
+let current = 0;
+
+setInterval(function () {
+    if (current === 0) {
+        current++;
+    }
+}, 1000);
+
+function Clearr() {
+    for (let i = 0; i < image.length; i++) {
+        Img[i].style.display = "none";
+    }
+}
+
+function Start() {
+    Clearr();
+    Img[0].style.display = "block";
+}
+
+function slideLeft() {
+    Clearr();
+    Img[current - 1].style.display = "block";
+    current--;
+}
+
+function slideRight() {
+    Clearr();
+    Img[current + 1].style.display = "block";
+    current++;
+
+
+}
+
+Btnleft.addEventListener("click", () => {
+    if (current === 0) {
+        current = image.length;
+    }
+    slideLeft();
+})
+
+BtnRight.addEventListener("click", () => {
+    if (current === image.length - 1) {
+        current = -1;
+    }
+    slideRight();
+})
+
+
+Start();
